6012be859e04fb3644b31c4b7e08a2b8d1dc1be2
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-10-11 21:08:11 -0500

    Lab Complete

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -1,5 +1,29 @@
 package com.csc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
 public class Zipper {
+    public static <T> List<T> zip(List <T> listOne, List <T> listTwo) {
+        List<T> list = new ArrayList<>();
+
+        int sizeOfListOne = listOne.size();
+        int sizeOfListTwo = listTwo.size();
+        
+        int sizeOfLargest = Math.max(listOne.size(), listTwo.size());
+        
+        for (int i = 0; i < sizeOfLargest; i++) {
+            if (i < sizeOfListOne) {
+                list.add(listOne.get(i));
+            }
+
+            if (i < sizeOfListTwo) {
+                list.add(listTwo.get(i));
+            }
+        }
+
+        return list;
+    }
 
 }
--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -5,17 +5,97 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class ZipperTest {
 
-  Zipper zipper;
+    Zipper zipper;
 
-  @BeforeEach
-  void setUp() {
-    zipper = new Zipper();
-  }
+    @BeforeEach
+    void setUp() {
+        zipper = new Zipper();
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    void itWorks() {
+        assertEquals(true, true);
+    }
+
+    @Test
+    void testZipWithSameSizedLists() {
+        List<Integer> listOne = List.of(1, 3, 5);
+        List<Integer> listTwo = List.of(2, 4, 6);
+        
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> actual = Zipper.zip(listOne, listTwo);
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testZipWithFirstListLarger() {
+        List<Integer> listOne = List.of(1, 3, 5, 7);
+        List<Integer> listTwo = List.of(2, 4);
+        
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 7);
+        List<Integer> actual = Zipper.zip(listOne, listTwo);
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testZipWithSecondListLarger() {
+        List<Integer> listOne = List.of(1, 3);
+        List<Integer> listTwo = List.of(2, 4, 6, 8);
+        
+        List<Integer> expected = List.of(1, 2, 3, 4, 6, 8);
+        List<Integer> actual = Zipper.zip(listOne, listTwo);
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testZipWithEmptyFirstList() {
+        List<Integer> listOne = List.of();
+        List<Integer> listTwo = List.of(2, 4, 6);
+        
+        List<Integer> expected = List.of(2, 4, 6);
+        List<Integer> actual = Zipper.zip(listOne, listTwo);
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testZipWithEmptySecondList() {
+        List<Integer> listOne = List.of(1, 3, 5);
+        List<Integer> listTwo = List.of();
+        
+        List<Integer> expected = List.of(1, 3, 5);
+        List<Integer> actual = Zipper.zip(listOne, listTwo);
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testZipWithBothEmptyLists() {
+        List<Integer> listOne = List.of();
+        List<Integer> listTwo = List.of();
+        
+        List<Integer> expected = new ArrayList<>();
+        List<Integer> actual = Zipper.zip(listOne, listTwo);
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testZipWithStrings() {
+        List<String> listOne = List.of("Red", "Green", "Blue");
+        List<String> listTwo = List.of("White", "Black");
+        
+        List<String> expected = List.of("Red", "White", "Green", "Black", "Blue");
+        List<String> actual = Zipper.zip(listOne, listTwo);
+        
+        assertEquals(expected, actual);
+    }
 }
